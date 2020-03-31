bbba1d1bccf90bd2eb86a62484a6ae187467de1e
abhisheksharma <62939441+AbhishekSharma16@users.noreply.github.com>
2020-03-31 19:54:25 +0530

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,26 +1,23 @@
-#include"DynamicArray.cpp"
-#include<iostream>
 int main()
 {
     std::cout<<"hi"<<std::endl;
 
 	ARRAY a;
-	 a.Insert(1);
-	 a.Insert(2);
-	 a.Insert(1);
-	 a.Insert(13);
-	 a.Insert(14);
-	 a.Insert(16);
-	 a.Insert(10);
-	 a.Insert(10);
-	 a.Insert(14);
-	 a.Insert(13);
-	 a.Insert(1);
-	 a.Insert(100);
-	 a.Insert(14);
-	
-	 for(int i=0 ; i<a.ElementCounter ; i++)
-	 std::cout<<*(a.AR+i);
+	a.Insert(1);
+	a.Insert(2);
+	a.Insert(3);
+	a.Insert(3);
+	a.Insert(3);
+	a.Insert(3);
+	a.Insert(3);
+	a.Insert(3);
+	a.Insert(3);
+	a.Insert(3);
+    a.Insert(3);
+    for (int i=1;i<=a.ElementCounter;i++)
+    {
+        std::cout<<*(a.AR+i)<<"\n";//////////////////
+    }
 
     return 0;
 }
